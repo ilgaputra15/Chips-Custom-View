0228978e8383b596f16afc2cf046cc16e351eb1f
ilga yulian putra <ilga.putra15@gmail.com>
2020-06-01 23:53:39 +0700

    Initial Commit

---
+++ Chips Custom View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chips Custom View
+//
+//  Created by ilga yulian putra on 01/06/20.
+//  Copyright © 2020 ilga yulian putra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Chips Custom ViewTests/Chips_Custom_ViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  Chips_Custom_ViewTests.swift
+//  Chips Custom ViewTests
+//
+//  Created by ilga yulian putra on 01/06/20.
+//  Copyright © 2020 ilga yulian putra. All rights reserved.
+//
+
+import XCTest
+@testable import Chips_Custom_View
+
+class Chips_Custom_ViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
